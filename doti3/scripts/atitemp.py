ccbd24eeee63f30aab12864678cd990bd19b5765
Billy Rhoades <billyarhoades@gmail.com>
2014-02-23 13:54:23 -0600

    Added card temp script.

---
+++ doti3/scripts/atitemp.py
@@ -0,0 +1,44 @@
+from subprocess import *
+import re
+
+class Py3status:
+    """
+    Empty and basic py3status class.
+
+    NOTE: py3status will NOT execute:
+        - methods starting with '_'
+        - methods decorated by @property and @staticmethod
+
+    NOTE: reserved method names:
+        - 'kill' method for py3status exit notification
+        - 'on_click' method for click events from i3bar
+    """
+    def kill(self, i3status_output_json, i3status_config):
+        """
+        This method will be called upon py3status exit.
+        """
+        pass
+
+    def on_click(self, i3status_output_json, i3status_config, event):
+        """
+        This method will be called when a click event occurs on this module's
+        output on the i3bar.
+
+        Example 'event' json object:
+        {'y': 13, 'x': 1737, 'button': 1, 'name': 'empty', 'instance': 'first'}
+        """
+        pass
+
+    def empty(self, i3status_output_json, i3status_config):
+        """
+        This method will return an empty text message
+        so it will NOT be displayed on your i3bar.
+
+        If you want something displayed you should write something
+        in the 'full_text' key of your response.
+
+        See the i3bar protocol spec for more information:
+        http://i3wm.org/docs/i3bar-protocol.html
+        """
+        response = {'full_text': "GPU: " + re.sub( r'[\'b]+', '', str(check_output("aticonfig --odgt | awk '{match($0,\"Temperature - ([0-9.]+) C\", a)}END{print a[1]};'| xargs echo -n", shell=True))) + " °C", 'name': 'atitemp', 'instance': 'first'}
+        return (0, response)
