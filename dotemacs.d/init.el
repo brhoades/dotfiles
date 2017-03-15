0e293b56068597db1c05e1ee10fa47df496c78f6
Billy Rhoades <billy.rhoades@cerner.com>
2017-03-15 14:38:01 -0500

    Trying to lighten up vim. Killed emacs' flymake, which was causing constant CPU usage.

--- dotemacs.d/init.el
+++ dotemacs.d/init.el
@@ -5,7 +5,7 @@
 (package-initialize)
 
 ;;;;;;;;;;;;;;; Emacs Config
-;; Save all tempfiles in $TMPDIR/emacs$UID/                                   
+;; Save all tempfiles in $TMPDIR/emacs$UID/
 (defconst emacs-tmp-dir (format "~/.emacs.d/tmp/backups/"))
 (setq backup-directory-alist
       `((".*" . ,emacs-tmp-dir)))
@@ -13,20 +13,17 @@
       `((".*" ,emacs-tmp-dir t)))
 (setq auto-save-list-file-prefix
       emacs-tmp-dir)
+(add-hook 'focus-out-hook 'save-all)
 
 (define-coding-system-alias 'UTF-8 'utf-8)
 (define-coding-system-alias 'utf8 'utf-8)
 
 ;; Always save our buffers and load them on start
 (desktop-save-mode 1)
-(setq desktop-auto-save-timeout 180)
+(setq desktop-auto-save-timeout 300)
 
 ;; Save history
-(savehist-mode 1)
-
-;; smex
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(add-to-list 'desktop-locals-to-save 'buffer-undo-list)
 
 ;; This is your old M-x.
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
@@ -36,12 +33,68 @@
 (setq whitespace-style '(tabs newline tab-mark newline-mark))
 ; (set-face-attribute 'whitespace-space " " background nil :foreground "gray30")
 
-; ruby does wild stuff inside parens...
-(setq ruby-deep-indent-paren nil)
+;;smex (better M-x)
+(global-set-key [(meta x)] (lambda ()
+                             (interactive)
+                             (or (boundp 'smex-cache)
+                                 (smex-initialize))
+                             (global-set-key [(meta x)] 'smex)
+                             (smex)))
+
+(global-set-key [(shift meta x)] (lambda ()
+                                   (interactive)
+                                   (or (boundp 'smex-cache)
+                                       (smex-initialize))
+                                   (global-set-key [(shift meta x)] 'smex-major-mode-commands)
+                                   (smex-major-mode-commands)))
+
 
 ; https://github.com/syl20bnr/spacemacs/issues/2626
 ; Fix for some restored sessions causing buffer lists to break.
 ; (push '(persp . :never) frameset-filter-alist)
+;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;; Ruby Config
+; ruby does wild stuff inside parens...
+(setq ruby-deep-indent-paren nil)
+
+;; rbenv
+;; (require 'rbenv)
+;; (global-rbenv-mode)
+
+;; Syntax checking
+;; (require 'flymake-ruby)
+;; (add-hook 'ruby-mode-hook 'flymake-ruby-load)
+
+;; open a shell
+(global-set-key (kbd "C-c r r") 'inf-ruby)
+
+;; Rails projectile
+(add-hook 'projectile-mode-hook 'projectile-rails-on)
+
+;; robe
+;; (require 'robe)
+;; (add-hook 'ruby-mode-hook 'robe-mode)
+
+;; robe with rbenv
+;; (defadvice inf-ruby-console-auto (before rbenv-use-corresponding activate))
+
+;; intelligent completion
+;; (global-company-mode t)
+;; (push 'company-robe company-backends)
+
+;; execute irb
+;; (define-key inf-ruby-minor-mode-map (kbd "C-c C-z") 'run-ruby)
+;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;
+;; Custom Shortcuts
+; Open config
+(global-set-key (kbd "C-c C-x i") (lambda() (interactive)(find-file "~/.emacs.d")))
+
+; Open new org file
+(global-set-key (kbd "C-c C-x o") (lambda() (interactive)(find-file "~/Documents/org")))
+
 ;;;;;;;;;;;;;;;
 
 (require 'evil)
@@ -54,6 +107,7 @@
 (require 'projectile)
 (projectile-mode 1)
 (setq projectile-enable-caching t)
+(add-to-list 'projectile-globally-ignored-directories "node_modules")
 
 (require 'flx)
 (require 'flx-ido)
@@ -96,37 +150,40 @@
 ;;;;;;;;;;;;;;; Python
 
 ;; Flymake
-(when (load "flymake" t)
-  (defun flymake-pyflakes-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list "pyflakes" (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pyflakes-init)))
-
-(add-hook 'find-file-hook 'flymake-find-file-hook)
-(setq exec-path (cons "/Users/br046823/.pyenv/shims" exec-path))
-(delete '("\\.html?\\'" flymake-xml-init) flymake-allowed-file-name-masks)
+;; (when (load "flymake" t)
+;;   (defun flymake-pyflakes-init ()
+;;     (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;;                        'flymake-create-temp-inplace))
+;;            (local-file (file-relative-name
+;;                         temp-file
+;;                         (file-name-directory buffer-file-name))))
+;;       (list "pyflakes" (list local-file))))
+;;   (add-to-list 'flymake-allowed-file-name-masks
+;;                '("\\.py\\'" flymake-pyflakes-init)))
+;;
+;; (add-hook 'find-file-hook 'flymake-find-file-hook)
+;; (setq exec-path (cons "/Users/br046823/.pyenv/shims" exec-path))
+;; (;; delete '("\\.html?\\'" flymake-xml-init) flymake-allowed-file-name-masks)
 
 ;; Place temp files in a temp dir
-(setq flymake-run-in-place t)
-(setq temporary-file-directory "~/.emacs.d/flymake-tmp/")
+;; (setq flymake-run-in-place t)
+;; (setq temporary-file-directory "~/.emacs.d/flymake-tmp/")
 
 ;; jedi
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:setup-keys t)
+; (add-hook 'python-mode-hook 'jedi:setup)
+; (setq jedi:setup-keys t)
 ;;;;;;;;;;;;;;; End Python
 
 ;;;;;;;;;;;;;;; JSON mode
+'(setq js-indent-level 2)
 (add-hook 'json-mode-hook
           (lambda ()
             (make-local-variable 'js-indent-level)
             (setq js-indent-level 2)))
 ;;;;;;;;;;;;;;; End JSON
 
+;;;;;;;;;;;;;;; Coffeescript
+;; This gives you a tab of 2 spaces
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -135,6 +192,8 @@
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(auto-revert-remote-files t)
+ '(coffee-indent-like-python-mode t)
+ '(coffee-tab-width 2)
  '(compilation-message-face (quote default))
  '(custom-enabled-themes (quote (misterioso)))
  '(fci-rule-color "#3C3D37")
@@ -152,7 +211,7 @@
  '(magit-diff-use-overlays nil)
  '(package-selected-packages
    (quote
-    (timesheet el-get jedi json-mode markdown-mode smex bug-hunter pony-mode helm-projectile flx-ido projectile helm evil)))
+    (helm-smex scala-mode yaml-mode rbenv inf-ruby smex evil-smartparens ruby-hash-syntax coffee-mode timesheet el-get jedi json-mode markdown-mode bug-hunter pony-mode helm-projectile flx-ido projectile helm evil)))
  '(pos-tip-background-color "#A6E22E")
  '(pos-tip-foreground-color "#272822")
  '(ruby-align-to-stmt-keywords nil)
@@ -181,6 +240,18 @@
  '(vc-annotate-very-old-color nil)
  '(weechat-color-list
    (unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
+
+;; automatically clean up bad whitespace
+(setq whitespace-action '(auto-cleanup))
+
+;; only show bad whitespace
+(setq whitespace-style '(trailing space-before-tab indentation empty space-after-tab))
+
+;; evil mode
+
+;;;;;;;;;;;;;;;
+
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
