8dbf241183242d0f901dcc6f9f1bea8396908c4a
Billy Rhoades <billyarhoades@gmail.com>
2019-11-09 09:46:32 -0800

    Added workaround for ws tabs in gomode. Fixed gocode.

--- dotemacs.d/config/modes/go-mode.el
+++ dotemacs.d/config/modes/go-mode.el
@@ -2,14 +2,22 @@
           (lambda ()
             (add-hook 'before-save-hook 'gofmt-before-save)
             (setq tab-width 4)
-            (setq indent-tabs-mode 1))
+            (setq indent-tabs-mode 1)
+            (setq whitespace-style '(trailing)))
           )
 
 ; http://tleyden.github.io/blog/2014/05/22/configure-emacs-as-a-go-editor-from-scratch/
 
 (defun auto-complete-for-go ()
   (auto-complete-mode 1))
+
 (add-hook 'go-mode-hook 'auto-complete-for-go)
 
 (with-eval-after-load 'go-mode
    (require 'go-autocomplete))
+
+
+; https://github.com/flycheck/flycheck/issues/1523
+(let ((govet (flycheck-checker-get 'go-vet 'command)))
+  (when (equal (cadr govet) "tool")
+    (setf (cdr govet) (cddr govet))))
