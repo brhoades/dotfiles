7c1544aad90bd74eda834dbc6750f6d3c3825f68
Billy Rhoades <billyarhoades@gmail.com>
2019-11-09 09:26:54 -0800

    Added go autocomplete properly

--- dotemacs.d/config/modes/go-mode.el
+++ dotemacs.d/config/modes/go-mode.el
@@ -4,3 +4,12 @@
             (setq tab-width 4)
             (setq indent-tabs-mode 1))
           )
+
+; http://tleyden.github.io/blog/2014/05/22/configure-emacs-as-a-go-editor-from-scratch/
+
+(defun auto-complete-for-go ()
+  (auto-complete-mode 1))
+(add-hook 'go-mode-hook 'auto-complete-for-go)
+
+(with-eval-after-load 'go-mode
+   (require 'go-autocomplete))
