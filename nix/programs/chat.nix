1a820ee6c60c23f2187260f3be68c64f31df1f92
Billy J Rhoades II <billy@ngrok.com>
2020-11-29 21:56:42 -0800

    discord now off main channel

--- nix/programs/chat.nix
+++ nix/programs/chat.nix
@@ -2,11 +2,6 @@
 { pkgs, ... }:
 
 {
-  nixpkgs.config.packageOverrides = _pkgs: {
-    # XXX: until 0.12 hits nixos-unstable
-    discord = import ../../../nixpkgs/pkgs/applications/networking/instant-messengers/discord { inherit pkgs; };
-  };
-
   home.packages = with pkgs; [
     signal-desktop discord slack
   ];
