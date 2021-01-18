86faa9e19275705f745bcf4a598b6325d0f77711
Billy J Rhoades II <me@brod.es>
2021-01-18 13:02:18 -0800

    discord: off master nixpkgs again

--- nix/programs/chat.nix
+++ nix/programs/chat.nix
@@ -1,10 +1,9 @@
-
 { pkgs, ... }:
 
 {
   nixpkgs.config.packageOverrides = _pkgs: {
-    # For future discord upgrades...
-    # discord = import ../../../nixpkgs/pkgs/applications/networking/instant-messengers/discord { inherit pkgs; };
+    # XXX: discord update breaks everything.
+    discord = import ../../../nixpkgs/pkgs/applications/networking/instant-messengers/discord { inherit pkgs; };
   };
 
   home.packages = with pkgs; [
