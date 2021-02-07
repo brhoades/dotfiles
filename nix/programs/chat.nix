b44c150f9dd60e58a3b31bc907096527007d2ddf
Billy J Rhoades II <billy@ngrok.com>
2021-02-07 10:50:39 -0800

    discord: now using nixpkgs again

--- nix/programs/chat.nix
+++ nix/programs/chat.nix
@@ -3,7 +3,7 @@
 {
   nixpkgs.config.packageOverrides = _pkgs: {
     # XXX: discord update breaks everything.
-    discord = import ../../../nixpkgs/pkgs/applications/networking/instant-messengers/discord { inherit pkgs; };
+    # discord = import ../../../nixpkgs/pkgs/applications/networking/instant-messengers/discord { inherit pkgs; };
   };
 
   home.packages = with pkgs; [
