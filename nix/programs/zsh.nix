1cd99ed8dc21248dd00e81ecbf6130726201a739
Billy J Rhoades II <me@brod.es>
2021-03-07 10:18:13 -0800

    zsh: fix history clobber. p10k preinit failures won't load extendedhistory / sharehistory
    A subsequent exit from a shell with failure to load that option
    writes its history without respect to others.
    
    I've fixed this by adding the options to .zshenv which is loaded first
    (per some medium article which sounds decent). That's well prior to p10k
    instant prompt.

--- nix/programs/zsh.nix
+++ nix/programs/zsh.nix
@@ -46,10 +46,21 @@ in {
       share = true;
     };
 
-    initExtraBeforeCompInit = ''
+
+    envExtra = ''
       # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
       # Initialization code that may require console input (password prompts, [y/n]
       # confirmations, etc.) must go above this block; everything else may go below.
+
+      # https://medium.com/@rajsek/zsh-bash-startup-files-loading-order-bashrc-zshrc-etc-e30045652f2e
+      # .zshenv is sourced prior to .zshrc / zprofile
+      # enable history options before powerlevel10k to prevent preload failures
+      # from clobbering history
+      setopt extendedhistory
+      setopt sharehistory
+    '';
+
+    initExtraBeforeCompInit = ''
       if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
         source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
       fi
