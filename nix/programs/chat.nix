a15e5d5de2939ad762d05ef17fe8fcca05e83781
Billy J Rhoades II <me@brod.es>
2021-06-13 09:08:30 -0700

    disabled steam, added multiline dep to weechat

--- nix/programs/chat.nix
+++ nix/programs/chat.nix
@@ -14,6 +14,7 @@
           configure = { availablePlugins, ... }: {
             scripts = with super.weechatScripts; [
               wee-slack
+              multiline
             ];
           };
         };
--- nix/programs/steam.nix
+++ nix/programs/steam.nix
@@ -1,23 +1,22 @@
 { pkgs, ... }: {
-  nixpkgs.config.packageOverrides = pkgs: {
-    # XXX: unstable has lib32 deps broken.
-    steam = let
-      # steam = (import <nixos-20.03> { }).steam;
-      steam = (import <nixos> { }).steam;
-    in (steam.override {
+  # nixpkgs.config.packageOverrides = pkgs: {
+    # steam = let
+      # steam = (import <nixos> { }).steam;
+      # steam = (import <nixpkgs> { }).steam;
+    # in (steam.override {
       # nativeOnly = true; # use nixos-provided libraries, not steam's.
-      extraPkgs = pkgs:
-        with pkgs; [
-          mono
-          gtk3
-          gtk3-x11
-          libgdiplus
-          zlib
-          steamPackages.steam-fonts
-        ];
-      # nativeOnly = true;
-    });
-  };
+      # extraPkgs = pkgs:
+        # with pkgs; [
+          # mono
+          # gtk3
+          # gtk3-x11
+          # libgdiplus
+          # zlib
+          # steamPackages.steam-fonts
+        # ];
+    # });
+  # };
 
-  home.packages = with pkgs; [ steam steamPackages.steam-fonts ];
+  # programs.steam.enable = true;
+  # home.packages = with pkgs; [ steam steamPackages.steam-fonts ];
 }
