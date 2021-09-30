27da9bd9e83605ccb3c61dd9a1da288451f6acf4
Billy J Rhoades II <billy@ngrok.com>
2021-09-30 07:27:28 -0700

    reenabled steam

--- nix/programs/steam.nix
+++ nix/programs/steam.nix
@@ -1,23 +1,21 @@
-{ pkgs, ... }:
-{
-  # nixpkgs.config.packageOverrides = pkgs: {
-  # steam = let
-  # steam = (import <nixos> { }).steam;
-  # steam = (import <nixpkgs> { }).steam;
-  # in (steam.override {
-  # nativeOnly = true; # use nixos-provided libraries, not steam's.
-  # extraPkgs = pkgs:
-  # with pkgs; [
-  # mono
-  # gtk3
-  # gtk3-x11
-  # libgdiplus
-  # zlib
-  # steamPackages.steam-fonts
-  # ];
-  # });
-  # };
+{ pkgs, ... }: let
 
-  # programs.steam.enable = true;
-  # home.packages = with pkgs; [ steam steamPackages.steam-fonts ];
+      smlpkgs = import <nixos-unstable-small> {};
+in {
+  nixpkgs.config.packageOverrides = pkgs: {
+    steam = (pkgs.steam.override {
+      nativeOnly = true; # use nixos-provided libraries, not steam's.
+      extraPkgs = (_:
+        with smlpkgs; [
+          mono
+          gtk3
+          gtk3-x11
+          libgdiplus
+          zlib
+          steamPackages.steam-fonts
+        ]);
+    });
+  };
+
+  home.packages = with smlpkgs; [ steam steamPackages.steam-fonts ];
 }
