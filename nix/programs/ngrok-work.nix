7af828a3a615997933565df490d17ae56450cd01
Billy J Rhoades II <billy@ngrok.com>
2022-03-22 08:50:37 -0700

    nuke old ngrok aliases

--- nix/programs/ngrok-work.nix
+++ nix/programs/ngrok-work.nix
@@ -1,4 +1,10 @@
 { pkgs, ... }: {
+  programs.zsh = {
+    initExtraBeforeCompInit = ''
+      source "/home/aaron/work/ngrok/.cache/ngrok-host-shellhook"
+    '';
+  };
+
   home.packages = with pkgs; [ lorri ];
 
   services.lorri.enable = true;
