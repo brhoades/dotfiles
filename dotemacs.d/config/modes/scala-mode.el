f7133b1a64ce4878aa54f32db17902039232c3bf
Billy J Rhoades II <billy@ngrok.com>
2022-03-23 12:35:31 -0700

    add scala mode

---
+++ dotemacs.d/config/modes/scala-mode.el
@@ -0,0 +1,8 @@
+(use-package lsp-metals
+  :ensure t
+  :custom
+  ;; Metals claims to support range formatting by default but it supports range
+  ;; formatting of multiline strings only. You might want to disable it so that
+  ;; emacs can use indentation provided by scala-mode.
+  (lsp-metals-server-args '("-J-Dmetals.allow-multiline-string-formatting=off"))
+  :hook (scala-mode . lsp))
