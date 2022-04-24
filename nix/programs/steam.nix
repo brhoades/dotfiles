0e71045d739dedeb1c102c6f00c97422653153b9
Billy J Rhoades II <me@brod.es>
2022-04-24 07:16:44 -0700

    update steam.nix to remote deprecated fonts

--- nix/programs/steam.nix
+++ nix/programs/steam.nix
@@ -6,17 +6,9 @@ in {
   nixpkgs.config.packageOverrides = pkgs: {
     steam = (pkgs.steam.override {
       nativeOnly = true; # use nixos-provided libraries, not steam's.
-      extraPkgs = (_:
-        with smlpkgs; [
-          mono
-          gtk3
-          gtk3-x11
-          libgdiplus
-          zlib
-          steamPackages.steam-fonts
-        ]);
+      extraPkgs = (_: with smlpkgs; [ mono gtk3 gtk3-x11 libgdiplus zlib ]);
     });
   };
 
-  home.packages = with smlpkgs; [ steam steamPackages.steam-fonts ];
+  home.packages = with smlpkgs; [ steam ];
 }
