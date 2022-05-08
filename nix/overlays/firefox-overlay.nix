505d8a0f1e7fafdbd945452cee1a968f1f9dfce6
Billy J Rhoades II <me@brod.es>
2022-05-08 07:15:21 -0700

    pin firefox overlay

--- nix/overlays/firefox-overlay.nix
+++ nix/overlays/firefox-overlay.nix
@@ -1,9 +1,11 @@
 let
   # Change this to a rev sha to pin
-  moz-rev = "master";
+  # moz-rev = "master";
+  moz-ref = "e1f7540";
   moz-url = builtins.fetchTarball {
     url =
       "https://github.com/mozilla/nixpkgs-mozilla/archive/${moz-rev}.tar.gz";
+    sha256 = "0wlmf82p93py1vk89j4pv9c92is9zpr9va3f0xwqlh3l2j5nkl1i";
   };
   nightlyOverlay = (import "${moz-url}/firefox-overlay.nix");
 in nightlyOverlay
