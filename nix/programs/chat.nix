f1f41c3e2c3a967fe5e33ce79d0d20259e9a7c88
Billy J Rhoades II <me@brod.es>
2022-05-15 11:47:23 -0700

    chat: added ozone flags for signal and slack

--- nix/programs/chat.nix
+++ nix/programs/chat.nix
@@ -8,6 +8,31 @@
           scripts = with super.weechatScripts; [ wee-slack multiline ];
         };
       };
+
+      # wrappers below force ozone with applications' electron
+      slack = super.slack.overrideAttrs (old:
+        with pkgs; {
+          installPhase = old.installPhase + ''
+            rm $out/bin/slack
+
+            makeWrapper $out/lib/slack/slack $out/bin/slack \
+              --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
+              --prefix PATH : ${lib.makeBinPath [ pkgs.xdg-utils ]} \
+              --add-flags "--ozone-platform=wayland --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer"
+          '';
+        });
+
+      signal-desktop = super.signal-desktop.overrideAttrs (old:
+        with pkgs; {
+          installPhase = old.installPhase + ''
+            rm $out/bin/signal-desktop
+
+            makeWrapper $out/lib/Signal/signal-desktop $out/bin/signal-desktop \
+              --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
+              --prefix PATH : ${lib.makeBinPath [ pkgs.xdg-utils ]} \
+              --add-flags "--ozone-platform=wayland --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer"
+          '';
+        });
     })
   ];
 
