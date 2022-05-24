41c5f6e16bedc8ea72df4542e02d3a5f2b47a237
Billy J Rhoades II <me@brod.es>
2022-05-24 07:59:10 -0700

    chat: disable signal electron, makes it fail to render

--- nix/programs/chat.nix
+++ nix/programs/chat.nix
@@ -22,17 +22,17 @@
           '';
         });
 
-      signal-desktop = super.signal-desktop.overrideAttrs (old:
-        with pkgs; {
-          installPhase = old.installPhase + ''
-            rm $out/bin/signal-desktop
-
-            makeWrapper $out/lib/Signal/signal-desktop $out/bin/signal-desktop \
-              --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
-              --prefix PATH : ${lib.makeBinPath [ pkgs.xdg-utils ]} \
-              --add-flags "--ozone-platform=wayland --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer"
-          '';
-        });
+      # signal-desktop = super.signal-desktop.overrideAttrs (old:
+      #   with pkgs; {
+      #     installPhase = old.installPhase + ''
+      #       rm $out/bin/signal-desktop
+      # 
+      #            makeWrapper $out/lib/Signal/signal-desktop $out/bin/signal-desktop \
+      #             --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
+      #            --prefix PATH : ${lib.makeBinPath [ pkgs.xdg-utils ]} \
+      #           --add-flags "--ozone-platform=wayland --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer"
+      #      '';
+      #   });
     })
   ];
 
