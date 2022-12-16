f3171fe69d4b6ea53da66c8f2778cc9da1615174
Billy J Rhoades II <billy@ngrok.com>
2022-12-16 06:39:33 -0800

    emacs: add lisp-mode config

--- dotemacs.d/config/modes/lisp-mode.el
+++ dotemacs.d/config/modes/lisp-mode.el
@@ -6,3 +6,16 @@
 									  'company-complete
                                       (add-to-list 'company-backends 'company-lsp)
                                       'lsp-mode)))
+
+(add-hook 'emacs-lisp-mode-hook
+          (lambda ()
+            ;; Use spaces, not tabs.
+            (setq indent-tabs-mode nil)
+            ;; more sane argument alignment
+            (setq lisp-indent-function 'common-lisp-indent-function)
+            ;; Keep M-TAB for `completion-at-point'
+            (define-key flyspell-mode-map "\M-\t" nil)
+            ;; Pretty-print eval'd expressions.
+            (define-key emacs-lisp-mode-map
+              "\C-x\C-e" 'pp-eval-last-sexp)))
+; (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
