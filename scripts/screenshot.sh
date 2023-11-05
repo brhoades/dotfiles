6b746b03f5f47da4a894cd83129d2e7c8c06fa27
Billy J Rhoades II <me@brod.es>
2023-11-05 09:08:26 -0800

    add screenshot script + workflow

---
+++ scripts/screenshot.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# Opening mostly sourced from schway.sh
+shopt -s extglob
+BASEPATH="$HOME/Pictures/Screenshots"
+MONTHPATH="$BASEPATH/$(date "+%Y")/$(date "+%m")"
+
+[[ ! -d "$MONTHPATH" ]] && mkdir -p "$MONTHPATH"
+
+FILENAME="$MONTHPATH/$(date +"%Y%m%d_%H%M%S_%Z")-iaukea-ss.png"
+
+screencapture -o -W -i "$FILENAME"
+
+# osascript -e "set the clipboard to \"$FILENAME\""
+osascript \
+-e 'on run args' \
+  -e 'set the clipboard to POSIX file (first item of args)' \
+  -e end \
+  "$FILENAME"
