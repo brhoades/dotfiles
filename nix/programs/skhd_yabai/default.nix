aa80543cfc95bfc44fc333a90ad9b552afa0c3a3
Billy J Rhoades II <me@brod.es>
2023-11-05 08:53:35 -0800

    fix skhd_yabai module

--- nix/programs/skhd_yabai/default.nix
+++ nix/programs/skhd_yabai/default.nix
@@ -5,6 +5,6 @@
   home.file.".yabairc".source = ./dotyabairc;
   home.file.".config/yabai/yabairc".source = ./dotyabairc;
 
-  home.file."Library/LaunchAgents/com.koekeishiya.skhd.plist" = ./launchagents/com.koekeishiya.skhd.plist;
-  home.file."Library/LaunchAgents/com.koekeishiya.yabai.plist" = ./launchagents/com.koekeishiya.yabai.plist;
-]
+  home.file."Library/LaunchAgents/com.koekeishiya.skhd.plist".source = ./launchagents/com.koekeishiya.skhd.plist;
+  home.file."Library/LaunchAgents/com.koekeishiya.yabai.plist".source = ./launchagents/com.koekeishiya.yabai.plist;
+}
