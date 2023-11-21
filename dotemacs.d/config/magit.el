a3bff78f028b24a71b962353a7467c230fbb3759
Billy J Rhoades II <brhoades@rippling.com>
2023-11-21 07:50:14 -0800

    emacs: add git-link-master, default to it

--- dotemacs.d/config/magit.el
+++ dotemacs.d/config/magit.el
@@ -35,32 +35,3 @@
             (setq line-end nil)))
         (list line-start line-end)))))
 
-(defun git-link-main ()
- (interactive)
- (setq git-link-default-branch "main")
- (apply 'git-link (append '("origin") (git-link--get-region)))
- (setq git-link-default-branch nil))
-
-(global-set-key (kbd "C-c p y")
-                (lambda () (interactive) (apply 'git-link (append '("origin") (git-link--get-region)))))
-
-(global-set-key (kbd "C-c p Y")
-                (lambda () (git-link-main)))
-
-(defun git-link-canonical()
- (interactive)
- (setq git-link-use-commit 't)
- (apply 'git-link (append '("origin") (git-link--get-region)))
- (setq git-link-use-commit nil))
-
-(defun git-link-canonical()
- (interactive)
- (setq git-link-use-commit 't)
- (apply 'git-link (append '("origin") (git-link--get-region)))
- (setq git-link-use-commit nil))
-
-(global-set-key (kbd "C-c p y")
-                (lambda () (interactive) (apply 'git-link (append '("origin") (git-link--get-region)))))
-
-(global-set-key (kbd "C-c p Y")
-                (lambda () (git-link-main)))
--- dotemacs.d/config/modes/git-link.el
+++ dotemacs.d/config/modes/git-link.el
@@ -1,5 +1,29 @@
 (use-package git-link
   :ensure t
   :custom
-  (defun git-clip-link () (git-link))
-  )
+  (defun git-clip-link () (git-link)))
+
+(defun git-link-master ()
+ (interactive)
+ (setq git-link-default-branch "master")
+ (apply 'git-link (append '("origin") (git-link--get-region)))
+ (setq git-link-default-branch nil))
+
+
+(defun git-link-main ()
+ (interactive)
+ (setq git-link-default-branch "main")
+ (apply 'git-link (append '("origin") (git-link--get-region)))
+ (setq git-link-default-branch nil))
+
+(global-set-key (kbd "C-c p y")
+                (lambda () (interactive) (apply 'git-link (append '("origin") (git-link--get-region)))))
+
+(global-set-key (kbd "C-c p Y")
+                (lambda () (git-link-master)))
+
+(defun git-link-canonical()
+ (interactive)
+ (setq git-link-use-commit 't)
+ (apply 'git-link (append '("origin") (git-link--get-region)))
+ (setq git-link-use-commit nil))
