a3eeab6cd2b4862d3a22f563ebc3f64a25707d04
Billy J Rhoades II <me@brod.es>
2024-05-16 13:24:54 -0700

    fzf: force install fzf binary for user

--- nix/programs/fzf.nix
+++ nix/programs/fzf.nix
@@ -1,7 +1,9 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.fzf = {
     enable = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
   };
+
+   home.packages = with pkgs; [ fzf ];
 }
