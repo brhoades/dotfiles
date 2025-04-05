04e0b06f21b7a00e697c1c2c72e0e9eb98531259
Billy J Rhoades II <brhoades@rippling.com>
2025-04-05 12:17:02 -0700

    screenshots: fix screenshot to work on work mbp

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -2,6 +2,7 @@
 #
 # Opening mostly sourced from schway.sh
 shopt -s extglob
+[[ -z "$HOME" ]] && export HOME="$(pwd)"
 BASEPATH="$HOME/Pictures/Screenshots"
 MONTHPATH="$BASEPATH/$(date "+%Y")/$(date "+%m")"
 
@@ -10,9 +11,6 @@ MONTHPATH="$BASEPATH/$(date "+%Y")/$(date "+%m")"
 FILENAME="$MONTHPATH/$(date +"%Y%m%d_%H%M%S_%Z")-$(hostname)-ss.png"
 
 launchctl asuser $(id -u) bash -c "screencapture -S -o -W -s -i "$FILENAME""
+launchctl asuser $(id -u) bash -c "osascript -e 'on run args' -e 'set the clipboard to POSIX file (first item of args)' -e end '$FILENAME'"
 
-osascript \
-  -e 'on run args' \
-  -e 'set the clipboard to POSIX file (first item of args)' \
-  -e end \
-  "$FILENAME"
+echo "Wrote screenshot to $FILENAME and copied to clipboard for user $(id -u)."
