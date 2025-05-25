fe2d72cd351a675847b3c19e0f21b98e3abd4b0e
Billy J Rhoades II <me@brod.es>
2025-05-25 10:02:09 -0700

    nix: fix 1password module zsh integration

--- nix/programs/zsh/1password_module.nix
+++ nix/programs/zsh/1password_module.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [ fzf _1password-cli ];
 
-  programs.zsh.initExtraBeforeCompInit = ''
+  programs.zsh.initContent = with pkgs; lib.mkOrder 550 ''
     source "${toString ./op.bash}"
   '';
 }
