b57ee7b0b32474f326ad4ae7197438c0a8633ec0
Billy J Rhoades II <me@brod.es>
2025-07-26 11:19:54 -0700

    nixfmt

--- nix/programs/zsh/1password_module.nix
+++ nix/programs/zsh/1password_module.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [ fzf _1password-cli ];
 
-  programs.zsh.initContent = with pkgs; lib.mkOrder 550 ''
-    source "${toString ./op.bash}"
-  '';
+  programs.zsh.initContent = with pkgs;
+    lib.mkOrder 550 ''
+      source "${toString ./op.bash}"
+    '';
 }
